fcfaefd7336a5dd07b8a9b0c792c80ba2efbb4e4
royceruisheng <royceruisheng@gmail.com>
2021-09-08 17:17:49 +0800

    completed challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find(id)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @edited_task = Task.find(params[:id])
+  end
+
+  def update
+    @updated_task = Task.find(params[:id])
+    @updated_task.update(task_params)
+
+    redirect_to task_path(@updated_task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    if @task.save
+      redirect_to tasks_path
+    end
+  end
+
+  def destroy
+    @destroyed_task = Task.find(params[:id])
+    @destroyed_task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
